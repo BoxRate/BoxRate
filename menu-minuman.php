c1c9a11d72aaad37772312c79af12a55c9a44af5
Unknown <apratama918@gmail.com>
2018-06-01 11:44:54 +0700

    Tambah Fungsi Edit pada Minuman Secara langsung

--- menu-minuman.php
+++ menu-minuman.php
@@ -1,186 +1,158 @@
 <?php
-  include('login.php'); 
-    if (empty($_SESSION['username'])) {
-        header('location: login.html');
-    }
-
+include('login.php');
+if (empty($_SESSION['username'])) {
+header('location: login.html');
+}
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
   <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>Menu-Minuman</title>
-
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
     <!-- Custom styles for this template -->
     <link rel="stylesheet" type="text/css" href="css/shop-homepage.css">
-
   </head>
-
   <body >
-
     <!-- Navigation -->
-   <!-- Navigation -->
-     <nav class="navbar navbar-expand-lg navbar-dark bg-info fixed-top">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-info fixed-top">
       <div class="container">
         <a class="navbar-brand" href="index.php">
           <img id='image-logo' src="images/logo/BoxRate.png">
           <a class="user-login" style="color: white; font-family: times new romans;"><?php if (isset($_SESSION["store"])): ?>
-                  <?php  echo $_SESSION['store'];?>
-                   <?php endif ?>
-
-                <?php if (isset($_SESSION["username"]) and $_SESSION['name']==NULL): ?>
-                  <?php  echo $_SESSION['username'];?>
-                   <?php endif ?></a> </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <?php  echo $_SESSION['store'];?>
+            <?php endif ?>
+            <?php if (isset($_SESSION["username"]) and $_SESSION['name']==NULL): ?>
+            <?php  echo $_SESSION['username'];?>
+          <?php endif ?></a> </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive" style="font-family: times new romans;">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php">Beranda
-              </a>
-            </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php#footer">Tentang</a>
-            </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php#footer">Tanggapan</a>
-            </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php#footer">Kontak</a>
-            </li>
-          </ul>
-          <div class="dropdown">
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive" style="font-family: times new romans;">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Beranda
+                </a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php#footer">Tentang</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php#footer">Tanggapan</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php#footer">Kontak</a>
+              </li>
+            </ul>
+            <div class="dropdown">
               <label data-toggle='dropdown'>
                 <img id='image-user' src="images/user.png">
               </label>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" style="text-transform: capitalize;" href=""> <?php if (isset($_SESSION["name"])): ?>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" style="text-transform: capitalize;" href=""> <?php if (isset($_SESSION["name"])): ?>
                   <?php  echo $_SESSION['name'];?>
-                   <?php endif ?></a></li>
-              <li class="dropdown-divider"></li>
-              <li><a href="" class="dropdown-item">Bantuan ?</a></li>
-              <li><a class="dropdown-item" href="">Pengaturan</a></li>
-               <li class="dropdown-divider"></li>
-              <li ><a class="dropdown-item" href="index.php?logout='1'">Keluar</a></li>
-            </ul>
+                <?php endif ?></a></li>
+                <li class="dropdown-divider"></li>
+                <li><a href="" class="dropdown-item">Bantuan ?</a></li>
+                <li><a class="dropdown-item" href="">Pengaturan</a></li>
+                <li class="dropdown-divider"></li>
+                <li ><a class="dropdown-item" href="index.php?logout='1'">Keluar</a></li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
-
-    <!-- Page Content -->
-    <div class="container">
-
-      <div class="row">
-
-        <div class="col-lg-3">
-
-          <h1 class="my-4">Menu</h1>
-          <div class="list-group">
-            <a href="menu-makanan.php" class="list-group-item">&emsp;&nbsp;Makanan</a>
-            <a href="#" class="list-group-item">&#x27a4; Minuman</a>
-            <div class="dropdown">
-            <a href="#" class="list-group-item" data-toggle="dropdown">&emsp;&nbsp;Edit Menu</a>
-            <ul class="dropdown-content">
-              <li><a href="edit-makanan.php" class="list-drop">&emsp;&emsp;Edit Makanan</a></li>
-              <li><a href="edit-minuman.php" class="list-drop">&emsp;&emsp;Edit Minuman</a></li>
-            </ul>
-          </div>
-
-          </div>
-
-        </div>
-        <!-- /.col-lg-3 -->
-
-        <div class="col-lg-9">
-
-          <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner" role="listbox">
-              <div class="carousel-item active">
-                <img id="image-promo" class="d-block img-fluid" src="images/diskon/minuman-01.jpg" alt="First slide">
-              </div>
-              <div class="carousel-item">
-                <img id="image-promo" class="d-block img-fluid" src="images/diskon/minuman-02.jpg" alt="Second slide">
-              </div>
-              <div id="image-promo" class="carousel-item">
-                <img id="image-promo" class="d-block img-fluid" src="images/diskon/minuman-03.jpg" alt="Third slide">
+      </nav>
+      <!-- Page Content -->
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3">
+            <h1 class="my-4">Menu</h1>
+            <div class="list-group">
+              <a href="menu-makanan.php" class="list-group-item">&emsp;&nbsp;Makanan</a>
+              <a href="#" class="list-group-item">&#x27a4; Minuman</a>
+              <div class="dropdown">
+                <a href="#" class="list-group-item" data-toggle="dropdown">&emsp;&nbsp;Edit Menu</a>
+                <ul class="dropdown-content">
+                  <li><a href="edit-makanan.php" class="list-drop">&emsp;&emsp;Edit Makanan</a></li>
+                  <li><a href="edit-minuman.php" class="list-drop">&emsp;&emsp;Edit Minuman</a></li>
+                </ul>
               </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
           </div>
-
-          <div class="row">
-
-        <?php
-           $db = mysqli_connect("localhost", "root", "", "boxrate");
-            $storeid=$_SESSION['store_id'];
-           $sql= "SELECT * FROM menu WHERE ket='minuman' and store_id='$storeid'";
-           $result = mysqli_query($db, $sql);
-           while ($row=mysqli_fetch_array($result)) {
-            echo "<div class='col-lg-4 col-md-6 mb-4'>
-              <div class='card h-100'>
-                <a href='#'><img id='image-menu' class='card-img-top' src='images/minuman/".$row['image']."' alt=''></a>
-                <div class='card-body'>
-                  <h4 class='card-title'>
-                    <a href='#'>".$row['name']."</a>
-                  </h4>
-                  <h5>Rp. ".$row['price']."</h5>
-                  <p class='card-text'>".$row['description']."</p>
+          <!-- /.col-lg-3 -->
+          <div class="col-lg-9">
+            <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              </ol>
+              <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                  <img id="image-promo" class="d-block img-fluid" src="images/diskon/minuman-01.jpg" alt="First slide">
                 </div>
-                <div class='card-footer'>
-                  
+                <div class="carousel-item">
+                  <img id="image-promo" class="d-block img-fluid" src="images/diskon/minuman-02.jpg" alt="Second slide">
+                </div>
+                <div id="image-promo" class="carousel-item">
+                  <img id="image-promo" class="d-block img-fluid" src="images/diskon/minuman-03.jpg" alt="Third slide">
                 </div>
               </div>
-            </div>";
-          }
-          ?>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+            <div class="row">
+              <?php
+              $db = mysqli_connect("localhost", "root", "", "boxrate");
+              $storeid=$_SESSION['store_id'];
+              $sql= "SELECT * FROM menu WHERE ket='minuman' and store_id='$storeid'";
+              $result = mysqli_query($db, $sql);
+              while ($row=mysqli_fetch_array($result)) {
+              echo "<div class='col-lg-4 col-md-6 mb-4'>
+                <div class='card h-100'>
+                  <a href='#'><img id='image-menu' class='card-img-top' src='images/minuman/".$row['image']."' alt=''></a>
+                  <div class='card-body'>
+                    <h4 class='card-title'>
+                    <a href='#'>".$row['name']."</a>
+                    </h4>
+                    <h5>Rp. ".$row['price']."</h5>
+                    <p class='card-text'>".$row['description']."</p>
+                  </div>
+                  <div class='card-footer'>
+                    
+                  </div>
+                </div>
+              </div>";
+              }
+              ?>
+            </div>
+            <!-- /.row -->
           </div>
-          <!-- /.row -->
-
+          <!-- /.col-lg-9 -->
         </div>
-        <!-- /.col-lg-9 -->
-
-      </div>
-      <!-- /.row -->
-
-    </div>
-    <!-- /.container -->
-
-    <!-- Footer -->
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; BoxRate 2018</p>
+        <!-- /.row -->
       </div>
       <!-- /.container -->
-    </footer>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  </body>
-
-</html>
+      <!-- Footer -->
+      <footer class="py-5 bg-dark">
+        <div class="container">
+          <p class="m-0 text-center text-white">Copyright &copy; BoxRate 2018</p>
+        </div>
+        <!-- /.container -->
+      </footer>
+      <!-- Bootstrap core JavaScript -->
+      <script src="vendor/jquery/jquery.min.js"></script>
+      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    </body>
+  </html>
